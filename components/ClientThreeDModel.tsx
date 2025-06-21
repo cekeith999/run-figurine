ea4afd8646329facd9414a31a636b4e1797dadaf
cekeith999 <cekeith@usc.edu>
2025-06-20 18:01:02 -0700

    chore: initial clean Next.js project setup

--- components/ClientThreeDModel.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from "next/dynamic";
-
-const ThreeDModel = dynamic(() => import("./ThreeDModel"), { ssr: false });
-
-export default function ClientThreeDModel() {
-  return <ThreeDModel />;
-} 
