f35c0f2512b489693f73443ae17a3069090c86fd
cekeith999 <cekeith@usc.edu>
2025-06-20 18:11:51 -0700

    feat: add Hamsa mask 3D scene from Spline

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -6,7 +6,7 @@ export default function SplineScene() {
   return (
     <div className="w-full h-[400px] rounded-lg overflow-hidden">
       <Spline 
-        scene="https://prod.spline.design/your-scene-url-here/scene.splinecode"
+        scene="https://prod.spline.design/u-9yEB9oVPokx3h9/scene.splinecode"
         style={{ width: '100%', height: '100%' }}
       />
     </div>
